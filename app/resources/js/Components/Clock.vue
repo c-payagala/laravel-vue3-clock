db98a71966e998d40602dd0609676c4e4c03f8c6
Chathura Payagala <chathupayagala@gmail.com>
2023-08-20 00:15:01 +0530

    basic version of clock component added

---
+++ app/resources/js/Components/Clock.vue
@@ -0,0 +1,21 @@
+<script setup>
+    import { ref } from 'vue';
+    import moment from 'moment';
+
+    const time = ref(moment().format('LTS'));
+
+    setInterval(() => {
+        time.value = moment().format('LTS');
+    }, 1000);
+
+</script>
+
+<template>
+    <div class="flex justify-center items-center bg-gradient-to-br from-indigo-600 to-indigo-900 p-20">
+        <h1>Time: {{ time }}</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
